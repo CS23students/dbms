fb7620e2b42148db9ab3d390a45de1a00343edbf
CS23students <153345000+CS23students@users.noreply.github.com>
2025-04-28 22:50:19 +0530

    Create 9.sql

---
+++ 10-pl-sql-triggers/9.sql
@@ -0,0 +1,6 @@
+-- 9. Disable all triggers on a table and Enable again
+-- Disable all triggers on 'employee' table
+ALTER TABLE employee DISABLE ALL TRIGGERS;
+
+-- Enable all triggers on 'employee' table
+ALTER TABLE employee ENABLE ALL TRIGGERS;
